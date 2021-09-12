bf7b407d41f84fba652244fe15cd5e9951bee317
Manabendra <mkchakraborty5@gmail.com>
2021-09-13 03:49:03 +0600

    bugs fixed first commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Fetching data for product carts
 const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
@@ -8,36 +9,48 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+  // get products as an array
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    // product is an object
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `
+    <div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+        <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <h3>${product.rating.rate} Stars!!!</h3>
+      <h3>Rating Count: ${product.rating.count}</h3>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+      <button id="details-btn" onclick="displayDetails('${product.id}')" class="btn btn-danger">Details</button>
+    </div>
+    `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+// adding product to cart
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
 
+  // the updateTaxAndCharge function will update Tax & Charge amount
   updateTaxAndCharge();
+
+  // the updateTotal function will update total amount
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,7 +59,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
@@ -78,3 +91,30 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+
+
+// this function will fetch data for a specific product and will display that 
+// product details above product carts
+const displayDetails = id => {
+  const productDetailsContainer = document.getElementById('productDetails');
+  const url = `https://fakestoreapi.com/products/${id}`;
+  fetch(url).then(res => res.json()).then(data => {
+    productDetailsContainer.innerHTML = `
+    <div class="single-product">
+      <h3>${data.title}</h3>
+      <div>
+        <img class="product-image" src=${data.image}></img>
+      </div>
+      <p>Category: ${data.category}</p>
+      <p>${data.description}</p>
+      <h2>Price: $ ${data.price}</h2>
+      <h3>${data.rating.rate} Stars!!!</h3>
+      <h3>Rating Count: ${data.rating.count}</h3>
+      <button onclick="hideDetail()" class="btn btn-danger">Hide Details</button>
+    </div>
+  `;
+  });
+};
+
+// this function will close the product detail box
+const hideDetail = () => { document.getElementById('productDetails').textContent = ''; };
